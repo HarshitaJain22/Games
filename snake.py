f2f8ab5ed580065fb947798f59a2652834d2a7f2
HarshitaJain22 <60313621+HarshitaJain22@users.noreply.github.com>
2020-04-07 10:59:05 +0530

    Update snake.py

--- snake.py
+++ snake.py
@@ -5,16 +5,31 @@ pygame.init()
 
 window = pygame.display.set_mode((500,500))
 pygame.display.set_caption("Snake (hisssssssssssss)")
-
+run = True
+head_x = 60
+head_y = 100
+fps = 5
+clock = pygame.time.Clock()
+velocity_x = 0
+velocity_y = 0
 #def rand_num():
 fruit_x = random.randint(0,500)
 fruit_y = random.randint(0,500)
     #info = [fruit_x, fruit_y]
     #return info
+fcentre_x = (20*(fruit_x//20))+10
+fcentre_y = (20*(fruit_y//20))+10
+if (fcentre_x-10 == head_x) and (fcentre_y-10 == head_y):
+    fruit_x = random.randint(0,500)
+    fruit_y = random.randint(0,500)
+    fcentre_x = 20*(fruit_x//20)
+    fcentre_y = 20*(fruit_y//20)
 
 
-def generate_food(head_x, head_y, fruit_x, fruit_y):
+def generate_food(head_x, head_y):
     #fruitc = rand_num()
+    fruit_x = random.randint(0,500)
+    fruit_y = random.randint(0,500)
     fcentre_x = (20*(fruit_x//20))+10
     fcentre_y = (20*(fruit_y//20))+10
 
@@ -25,8 +40,8 @@ def generate_food(head_x, head_y, fruit_x, fruit_y):
         fcentre_y = 20*(fruit_y//20)
 
     pygame.draw.circle(window, (255,0,0), (fcentre_x, fcentre_y), 10)
-    fruit_coordinates = [fcentre_x, fcentre_y]
-    return fruit_coordinates
+    #fruit_coordinates = [fcentre_x, fcentre_y]
+    #return fruit_coordinates
 
 def make_grid():
     window.fill((255,255,255))
@@ -45,25 +60,18 @@ def check_interaction(position,fruitx, fruity):
     check_y = position[1]//20
     food_x = fruitx-10
     food_y = fruity-10
-    10
     if (check_x == 25) or (check_y == 25) or (check_x == 0) or (check_y == 0):
         print("You Lost")
         return False
     elif (check_x == food_x) and (check_y == food_y):
         make_grid()
-        generate_food(position[0], position[1],fruitx, fruity)
+        generate_food(position[0], position[1])
         return True
     else:
         return True
 
 
-run = True
-head_x = 60
-head_y = 100
-fps = 5
-clock = pygame.time.Clock()
-velocity_x = 0
-velocity_y = 0
+
 
 while run:
     for event in pygame.event.get():
@@ -89,11 +97,12 @@ while run:
                 velocity_y = 20
 
     make_grid()
-    fruit_info = generate_food(head_x, head_y, fruit_x, fruit_y)
+    pygame.draw.circle(window, (255,0,0), (fcentre_x, fcentre_y), 10)
+    #fruit_info = generate_food(head_x, head_y)
     head_x += velocity_x
     head_y += velocity_y
     head_pos = [head_x, head_y]
-    run = check_interaction(head_pos, fruit_info[0], fruit_info[1])
+    run = check_interaction(head_pos, fcentre_x, fcentre_y)
     pygame.draw.rect(window, (0, 255,0) , (head_x, head_y, 20, 20))
     clock.tick(fps)
     pygame.display.update()
