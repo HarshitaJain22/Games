e485f6601766c6f93b21c30a40036c0bfd39960b
HarshitaJain22 <60313621+HarshitaJain22@users.noreply.github.com>
2020-04-14 11:38:52 +0530

    Update snake.py

--- snake.py
+++ snake.py
@@ -10,14 +10,17 @@ run = True
  
 head_x = 80
 head_y = 100
- 
+head_pos = [head_x, head_y] 
 body_1x = 60
 body_1y = 100
-
-body_2x = 60
+body_1pos = [body_1x, body_1y]
+body_2x = 40
 body_2y = 100
+body_2pos = [body_2x, body_2y]
 
-fps = 1
+body_c = [head_pos, body_1pos, body_2pos]
+
+fps = 3
 clock = pygame.time.Clock()
  
 velocity_x = 0
@@ -33,6 +36,26 @@ def make_grid():
         y = y + diff
         pygame.draw.line(window, (0,0,0), (x,0), (x,500))
         pygame.draw.line(window, (0,0,0), (0,y), (500,y))
+
+
+def coordinates(bodyco, velx, vely):
+    bodyc = bodyco
+    bodyc[-1][0] = bodyc[-2][0]
+    bodyc[-1][1] = bodyc[-2][1]
+    bodyc[-2][0] = bodyc[-3][0]
+    bodyc[-2][1] = bodyc[-3][1]
+    bodyc[-3][0] += velx
+    bodyc[-3][1] += vely
+    return bodyc
+
+
+def draw_body(window, bodycor, velox, veloy):
+    body = coordinates(bodycor, velox, veloy)
+    pygame.draw.rect(window, (0, 255,0) , (body[2][0], body[2][1], 20, 20))
+    pygame.draw.rect(window, (0, 255,0) , (body[1][0], body[1][1], 20, 20))    
+    pygame.draw.rect(window, (0, 255,0) , (body[0][0], body[0][1], 20, 20))
+    pygame.draw.rect(window, (0, 0,0) , (body[0][0]+4, body[0][1]+4, 12,12))
+    
 
 
 while run:
@@ -60,17 +83,8 @@ while run:
 
                  
     make_grid()
-    body_2x = body_1x
-    body_2y = body_1y
-    body_1x = head_x
-    body_1y = head_y
-    head_x += velocity_x
-    head_y += velocity_y
-    head_pos = [head_x, head_y]
-    pygame.draw.rect(window, (0, 255,0) , (head_x, head_y, 20, 20))
-    pygame.draw.rect(window, (0, 0,0) , (head_x+4, head_y+4, 12,12))
-    pygame.draw.rect(window, (0, 255,0) , (body_1x, body_1y, 20, 20))
-    pygame.draw.rect(window, (0, 255,0) , (body_2x, body_2y, 20, 20))
+    draw_body(window, body_c, velocity_x, velocity_y)
+    pygame.draw.rect(window, (0, 0,255) , (head_x, head_y, 20, 20)) #serves the purpose of house/starting point
     pygame.display.update()
     clock.tick(fps)
  
